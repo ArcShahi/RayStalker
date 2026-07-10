a280aaa783b5fad07d7f308dae4e6cf3d8287a4c
ArcShahi <arcshahi54@gmail.com>
2026-07-10 15:37:49 +0530

    Light emission

--- RayStalker/include/material.hpp
+++ RayStalker/include/material.hpp
@@ -5,7 +5,11 @@
 struct Material
 {
 	glm::vec3 Albedo{ 1.0f };
+	glm::vec3 EmissionColor{ 0.0f };
 	float roughness{1.0};
-	float metallic{ 0.0 };
+	float metallic{ 0.0f };
+	float EmissionPower{ 0.0f };
+
+	glm::vec3 GetEmission()const { return EmissionColor * EmissionPower; }
 };
 #endif // !MATERIAL_HPP
